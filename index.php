e7f3637721a41bf176f1b3a01a598b4c3ec63d0e
Narekm08 <145570643+Narekm08@users.noreply.github.com>
2023-09-20 15:09:18 +0200

    index html met menu en style

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Kledingwinkel</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+<h1>Welkom Smith & Oak</h1>
+<nav>
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">Dames</a></li>
+<li><a href="#">Heren</a></li>
+<li><a href="#">Kinderen</a></li>
+<li><a href="#">Over ons</a></li>
+<li><a href="#">Contact</a></li>
+
+<title>Font Awesome Icons</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+ 
+
+
+
+ 
+
+
+
+
+ 
+
+
+<button style="font-size:16px">Winkelwagen <i class="fa fa-shopping-cart"></i></button> 
+</ul>
+</nav>
+  
+</header>
+<main>
+<section class="product">
+<img src="images/product1.jpg" alt="Kledingstuk 1">
+<h2>Kledingstuk 1</h2>
+<p>Beschrijving van het kledingstuk.</p>
+<p>Prijs: $XX.XX</p>
+<button>Toevoegen aan winkelwagen</button>
+</section>
+
+
+    
+    
+<!-- Voeg meer productsecties toe zoals hierboven -->
+</main>
+<footer>
+<p>&copy; 2023 Smith & Oak</p>
+</footer>
+</body>
+</html>
