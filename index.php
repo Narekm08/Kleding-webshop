197579f47d90867d5d133490a3a2cd66defe3780
fortnitter <145571254+fortnitter@users.noreply.github.com>
2023-10-18 14:29:52 +0200

    video als bron

--- index.php
+++ index.php
@@ -165,7 +165,7 @@ span.psw {
 </header>
 <main>
  <video width="320" height="240"  loop autoplay muted>
-  <source src="movie.mp4" type="video/mp4">
+  <source src="video.mp4" type="video/mp4">
   Your browser does not support the video tag.
 </video>
 <section class="product">
