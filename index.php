1bc9c6a3d9d1f9203ff1476d242e1ed7c8b808a9
Narekm08 <145570643+Narekm08@users.noreply.github.com>
2023-10-30 10:43:04 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -4,170 +4,41 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Kledingwinkel</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="style.css">
+   <script>
+
+function Scrolldown() {
+    window.location.hash = '#iWantToScrollHere';
+}
+</script>
 </head>
+    
 <body>
 <header>
+
 <h1>Welkom Smith & Oak</h1>
+  
 <nav>
 <ul>
 <li><a href="#">Home</a></li>
-<li><a href="#">Dames</a></li>
+    
+    
+    
+    
+    
+<li><a href="#" onclick="scrolldown()">Dames</a></li>
 <li><a href="#">Heren</a></li>
 <li><a href="#">Kinderen</a></li>
 <li><a href="#">Over ons</a></li>
 <li><a href="#">Contact</a></li>
-
-<title>Font Awesome Icons</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-
- 
-
-
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {font-family: Arial, Helvetica, sans-serif;}
-
-/* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-}
-
-/* Set a style for all buttons */
-button {
-  background-color: #04AA6D;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-}
-
-button:hover {
-  opacity: 0.8;
-}
-
-/* Extra styles for the cancel button */
-.cancelbtn {
-  width: auto;
-  padding: 10px 18px;
-  background-color: #f44336;
-}
-
-/* Center the image and position the close button */
-.imgcontainer {
-  text-align: center;
-  margin: 24px 0 12px 0;
-  position: relative;
-}
-
-img.avatar {
-  width: 40%;
-  border-radius: 50%;
-}
-
-.container {
-  padding: 16px;
-}
-
-span.psw {
-  float: right;
-  padding-top: 16px;
-}
-
-/* The Modal (background) */
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-  padding-top: 60px;
-}
-
-/* Modal Content/Box */
-.modal-content {
-  background-color: #fefefe;
-  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
-  border: 1px solid #888;
-  width: 80%; /* Could be more or less, depending on screen size */
-}
-
-/* The Close Button (x) */
-.close {
-  position: absolute;
-  right: 25px;
-  top: 0;
-  color: #000;
-  font-size: 35px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: red;
-  cursor: pointer;
-}
-
-/* Add Zoom Animation */
-.animate {
-  -webkit-animation: animatezoom 0.6s;
-  animation: animatezoom 0.6s
-}
-
-@-webkit-keyframes animatezoom {
-  from {-webkit-transform: scale(0)} 
-  to {-webkit-transform: scale(1)}
-}
-  
-@keyframes animatezoom {
-  from {transform: scale(0)} 
-  to {transform: scale(1)}
-}
-
-/* Change styles for span and cancel button on extra small screens */
-@media screen and (max-width: 300px) {
-  span.psw {
-     display: block;
-     float: none;
-  }
-  .cancelbtn {
-     width: 100%;
-  }
-
-
-
-
-
- 
-
-
+    
 <button style="font-size:16px">Winkelwagen <i class="fa fa-shopping-cart"></i></button> 
 </ul>
 </nav>
   
 </header>
 <main>
- <video width="320" height="240"  loop autoplay muted>
-  <source src="video.mp4" type="video/mp4">
-  Your browser does not support the video tag.
-</video>
 <section class="product">
 <img src="images/product1.jpg" alt="Kledingstuk 1">
 <h2>Kledingstuk 1</h2>
@@ -175,14 +46,16 @@ span.psw {
 <p>Prijs: $XX.XX</p>
 <button>Toevoegen aan winkelwagen</button>
 </section>
-
-
-   
     
+    <?php
+   echo  $_COOKIE['broek'];
+echo $_COOKIE['trui'];
+    
+    ?>   
 <!-- Voeg meer productsecties toe zoals hierboven -->
 </main>
 <footer>
-<p>&copy; 2023 Smith & Oak</p>
+<p id="iWantToScrollHere" name="iWantToScrollHere">&copy; 2023 Smith & Oak</p>
 </footer>
 </body>
 </html>
