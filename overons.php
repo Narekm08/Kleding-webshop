e10763dc678a32ceb30fefd45f331476a7538831
Narekm08 <145570643+Narekm08@users.noreply.github.com>
2023-11-08 14:21:48 +0100

    Add files via upload

---
+++ overons.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Kledingwinkel</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="style.css">
+   <script>
+
+    
+    }
+    </script>
+        
+    <style>
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 33.3%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin: 8px;
+}
+
+.about-section {
+  padding: 50px;
+  text-align: center;
+  background-color: #474e5d;
+  color: white;
+}
+
+.container {
+  padding: 0 16px;
+}
+
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: #555;
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+        
+        
+        
+        
+        
+        
+    </style>
+</head>
+    
+<body>
+<header>
+<h1>Welkom Smith & Oak</h1>
+  
+<nav>
+<ul>
+<li><a href="index.php">Home</a></li>
+
+    
+    
+    
+ 
+<li><a href="dames.php">Dames</a></li>
+<li><a href="heren.php">Heren</a></li>
+<li><a href="kinderen.php">Kinderen</a></li>
+<li><a href="overons.php">Over ons</a></li>
+<li><a href="contact.php">Contact</a></li>
+    
+<button style="font-size:16px">Winkelwagen <i class="fa fa-shopping-cart"></i></button> 
+</ul>
+</nav>
+  
+</header>
+<main>
+
+<div class="about-section">
+  <h1>Over ons</h1>
+  <p>Wij hebben een kledingwinkel opgericht genaamd: Smith & Oak.</p>
+  <p>Deze website staat voor pure luxe.</p>
+</div>
+
+<h2 style="text-align:center">Ons Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+    
+      <div class="container">
+        <h2>Roelof</h2>
+        <p class="title">Scrum master</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      
+      <div class="container">
+        <h2>Narek</h2>
+        <p class="title">Product owner</p>
+       
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      
+      <div class="container">
+        <h2>Maarten</h2>
+        <p class="title">Designer</p>
+       
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+  
+  
+  
+  
+  
+  
+  <div class="column">
+    <div class="card">
+      
+      <div class="container">
+        <h2>Nahom</h2>
+        <p class="title">Designer</p>
+        
+        
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>  
+        
+        
+        
+        
+
+
+
+     
+<!-- Voeg meer productsecties toe zoals hierboven -->
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
